3e6c61c4176df473a58cba53ef02380053860a81
Albertto Castro <albertto.programmer@gmail.com>
2020-04-10 20:06:08 -0300

    Initiates theme files

--- front-page.php
+++ front-page.php
@@ -2,6 +2,8 @@
     get_header();
 ?>
 
+<?php echo wp_enqueue_style( 'cvupdates-stylesheet', get_template_directory_uri() . '/assets/css/app.css' ); ?>
+
 <?php
     get_footer();
 ?>
--- functions.php
+++ functions.php
@@ -1,1 +1,10 @@
 <?php
+
+function cvupdates_scripts() {
+    // wp_enqueue_style( 'style', get_stylesheet_uri() );
+    // wp_enqueue_script( 'app' , get_theme_file_uri( 'assets/js/' ));
+    wp_enqueue_style( 'cvupdates-stylesheet', get_template_directory_uri() . '/assets/css/app.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'cvupdates_scripts' );
+?>
