a98f422d1cb43c6e72d0b3aac23926fd76347d46
Albertto Castro <albertto.programmer@gmail.com>
2020-04-11 17:42:02 -0300

    Shows brazilian numbers

--- front-page.php
+++ front-page.php
@@ -19,6 +19,87 @@
     </div>    
 </div>
 
+<div id="brazil-last-data" class="container">
+    <?php $brazil_data = cvupdates_data_brazil(); ?>
+    <h1 class="text-center">Últimos números no Brasil</h1>
+    <div class="card bg-light text-dark text-center p-3 mb-1">
+        <blockquote class="blockquote mb-1">
+            <div class="text-center text-dark mb-1">
+                <span class="h3">Números Gerais</span>
+            </div>
+            <div class="d-flex justify-content-between text-success">
+                <span>Recuperados</span>
+                <span><?php echo $brazil_data['recovered'] ?></span>
+            </div>
+            <div class="d-flex justify-content-between text-warning">
+                <span>Casos</span>
+                <span><?php echo $brazil_data['cases'] ?></span>
+            </div>
+            <div class="d-flex justify-content-between text-danger">
+                <span>Mortes</span>
+                <span><?php echo $brazil_data['deaths'] ?></span>
+            </div>
+        </blockquote>
+        <blockquote class="blockquote mb-0">
+            <div class="text-center text-dark mb-1">
+                <span class="h3">Hoje</span>
+            </div>
+            <div class="d-flex justify-content-between text-warning">
+                <span>Casos</span>
+                <span><?php echo $brazil_data['todayCases'] ?></span>
+            </div>
+            <div class="d-flex justify-content-between text-danger">
+                <span>Mortes</span>
+                <span><?php echo $brazil_data['todayDeaths'] ?></span>
+            </div>
+        </blockquote>
+    </div>
+</div>
+
+<div id="brazil-evolution-chart" class="container card bg-light mx-1">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <style src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"></style>
+    <canvas id="lineChart" class="w-75"></canvas>
+    <script>
+        //line
+        <?php $chart = cvupdates_progression_chart() ?>
+        var ctxL = document.getElementById("lineChart").getContext('2d');
+        var myLineChart = new Chart(ctxL, {
+        type: 'line',
+        data: {
+        labels: <?php echo json_encode($chart['labels']) ?>,
+        datasets: [
+            {
+                label: "Casos",
+                data: <?php echo json_encode($chart['data']['cases'], JSON_HEX_TAG) ?>,
+                backgroundColor: [
+                    'rgba(105, 0, 132, .2)',
+                ],
+                borderColor: [
+                    'rgba(200, 99, 132, .7)',
+                ],
+                borderWidth: 2
+            },
+            {
+                label: "Mortes",
+                data: <?php echo json_encode($chart['data']['deaths']) ?>,
+                backgroundColor: [
+                    'rgba(0, 137, 132, .2)',
+                ],
+                borderColor: [
+                    'rgba(0, 10, 130, .7)',
+                ],
+                borderWidth: 2
+            }
+        ]
+        },
+        options: {
+            responsive: true
+        }
+        });
+    </script>
+</div>
+
 <!-- /Body -->
 <?php
     get_footer();
--- functions.php
+++ functions.php
@@ -7,4 +7,50 @@ function cvupdates_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'cvupdates_scripts' );
+
+
+
+function cvupdates_http($type = 'get', $url = ''){
+    // Initializating cUrl for http operations
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_URL => $url
+    ]);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    return $response;
+}
+
+function cvupdates_data_brazil() {
+    $api = 'https://corona.lmao.ninja/countries/Brazil';
+    return json_decode(cvupdates_http('get', $api), true);
+}
+
+function cvupdates_progression_brazil() {
+    $api = 'https://corona.lmao.ninja/v2/historical/Brazil';
+    return json_decode(cvupdates_http('get', $api), true);
+}
+
+function cvupdates_progression_chart() {
+    $api_response = cvupdates_progression_brazil();
+    
+    $chart = [];
+
+    $chart["labels"] = array_keys($api_response['timeline']['cases']);
+    
+    $chart['data'] = [];
+    $chart['data']['cases'] = array_values(
+        $api_response['timeline']['cases']
+    );
+    $chart['data']['deaths'] = array_values(
+        $api_response['timeline']['deaths']
+    );
+    $chart['data']['recovered'] = array_values(
+        $api_response['timeline']['recovered']
+    );
+
+    return $chart;
+}
 ?>
